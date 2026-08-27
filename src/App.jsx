9b97f35a305667b6f7ec2e56d4f1240cfea3a40e
Jesús González Gómez <gonzalezgomezjesus16061997@gmail.com>
2026-08-27 18:26:16 +0200

    feat: render TitleDescriptionComponent and CategoryCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './styles/App.css'
 import Footer from './components/footer/Footer.jsx'
+import CategoryCard from './components/CategoryCard/CategoryCard.jsx'
+import TitleDescriptionComponent from './components/TitleDescriptionComponent/TitleDescriptionComponent.jsx'
 import Weather from './components/wather/weather.jsx'
 
 function App() {
 	return (
 		<main>
 			<Weather />
+			<TitleDescriptionComponent
+				subtitle='QUANTUM DEPARTMENT'
+				subtitleColor='cyan'
+				title='Categorías de Productos'
+				description='Ponemos a tu disposición una oferta diversificada de productos'
+			/>
+			<CategoryCard />
 			<Footer />
 		</main>
 	)
