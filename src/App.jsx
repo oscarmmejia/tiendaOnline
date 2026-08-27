ce655e69f1b4df2d48e828cb30b9d4b35cf89f4c
moigarcia00 <moisesgarsanz@gmail.com>
2026-08-27 17:48:03 +0200

    docs: delete test button

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
-import { useState } from 'react'
 import './styles/App.css'
 import Footer from './components/footer/Footer.jsx'
-import Team from './components/pages/team.jsx'
 import Weather from './components/wather/weather.jsx'
 
 function App() {
-	const [showTeam, setShowTeam] = useState(false)
-
 	return (
-		<>
-			<button className="team-toggle" type="button" onClick={() => setShowTeam(!showTeam)}>
-				{showTeam ? 'Ver clima' : 'Ver equipo'}
-			</button>
-			{showTeam ? <Team /> : (
-				<main>
-					<Weather />
-					<Footer />
-				</main>
-			)}
-		</>
+		<main>
+			<Weather />
+			<Footer />
+		</main>
 	)
 }
 
