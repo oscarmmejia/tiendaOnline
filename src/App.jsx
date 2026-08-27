a7e002c2c89d017d70dd87da4c71dfbec34a930f
moigarcia00 <moisesgarsanz@gmail.com>
2026-08-27 17:33:31 +0200

    feat: add and modify cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react'
 import './styles/App.css'
 import Footer from './components/footer/Footer.jsx'
+import Team from './components/pages/team.jsx'
 import Weather from './components/wather/weather.jsx'
 
 function App() {
+	const [showTeam, setShowTeam] = useState(false)
+
 	return (
-		<main>
-			<Weather />
-			<Footer />
-		</main>
+		<>
+			<button className="team-toggle" type="button" onClick={() => setShowTeam(!showTeam)}>
+				{showTeam ? 'Ver clima' : 'Ver equipo'}
+			</button>
+			{showTeam ? <Team /> : (
+				<main>
+					<Weather />
+					<Footer />
+				</main>
+			)}
+		</>
 	)
 }
 
