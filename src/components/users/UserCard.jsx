8e4793659eb40c40c18f1977b0c4205680ba9e0b
Jesús González Gómez <gonzalezgomezjesus16061997@gmail.com>
2026-08-31 22:05:06 +0200

    feat: add edit buttom and modal to user cards

--- src/components/users/UserCard.jsx
+++ src/components/users/UserCard.jsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
+import Modal from "../modal/Modal";
 import "./UserCard.css";
 
 const UserCard = ({ avatar, name }) => {
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+
+  const handleOpenEditModal = () => {
+    setIsEditModalOpen(true);
+  };
+
+  const handleCloseEditModal = () => {
+    setIsEditModalOpen(false);
+  };
+
   return (
-    <div className="userCard">
-      <div className="userCardAvatarWrapper">
-        <img src={avatar} alt={name} className="userCardAvatar" />
+    <>
+      <div className="userCard">
+        <div className="userCardAvatarWrapper">
+          <img src={avatar} alt={name} className="userCardAvatar" />
+        </div>
+        <h3 className="userCardName">{name}</h3>
+        <button
+          className="userCardEditButton"
+          onClick={handleOpenEditModal}
+          aria-label={`Editar usuario ${name}`}
+        >
+          Editar
+        </button>
       </div>
-      <h3 className="userCardName">{name}</h3>
-    </div>
+
+      <Modal isOpen={isEditModalOpen} onClose={handleCloseEditModal}>
+        {/* Modal content will be added here */}
+      </Modal>
+    </>
   );
 };
 
