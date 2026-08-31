da9300d6cfe632b207d78879a34110fcd7dfc36d
Jesús González Gómez <gonzalezgomezjesus16061997@gmail.com>
2026-08-31 23:55:24 +0200

    feat: add edit buttom on Product Card and his Modal

---
+++ src/components/molecules/productEditModal/ProductEditModal.jsx
@@ -0,0 +1,7 @@
+import Modal from '../../modal/Modal'
+
+const ProductEditModal = ({ isOpen, onClose }) => {
+	return <Modal isOpen={isOpen} onClose={onClose} />
+}
+
+export default ProductEditModal
