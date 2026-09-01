c3096c7cf09c53f7a7b7e685432c73113d960563
cristinarodriguezl-dev <cristinarodriguez.cv@gmail.com>
2026-09-01 11:37:49 +0200

    feat: implement service and button for deleting users

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,11 +1,30 @@
+import { useEffect } from "react";
 import { useUsers } from "../../hooks/useUsers";
-import UserCard from "./UserCard";
+import { login } from "../../services/httpClient";
+import { deleteUser } from "../../services/userService";
+import { UserCard } from "./UserCard";
 import "./Users.css";
 
 const USERS_LIMIT = 8;
 
 const Users = () => {
     const { users, loading, error, refresh } = useUsers(USERS_LIMIT);
+    useEffect(() => {
+        const init = async () => {
+            try {
+                await login("[EMAIL_ADDRESS]", "12345678");
+            } catch (error) {
+                console.error("Login failed:", error);
+            }
+        };
+
+        init();
+    }, []);
+    const handleDelete = async (id) => {
+        await login("john@mail.com", "changeme");
+        await deleteUser(id);
+        refresh();
+    };
 
     return (
         <section className="users">
@@ -29,6 +48,7 @@ const Users = () => {
                             avatar={user.avatar}
                             name={user.name}
                             onUserUpdated={refresh}
+                            onDelete={handleDelete}
                         />
                     ))}
                 </div>
